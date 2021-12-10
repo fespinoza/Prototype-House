bb4398c2887a16de13973ec7d4adc00283719caf
Felipe Espinoza <fespinozacast@gmail.com>
2021-12-10 12:51:50 +0100

    Initial Commit

---
+++ Prototype House/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prototype House
+//
+//  Created by Felipe Espinoza on 10/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prototype House/Prototype_HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_HouseApp.swift
+//  Prototype House
+//
+//  Created by Felipe Espinoza on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
