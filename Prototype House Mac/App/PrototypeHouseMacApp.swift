b746f5c69c45ab555255160f77f9aead36fd41f3
Felipe Espinoza <fespinozacast@gmail.com>
2022-01-03 14:51:51 +0100

    Add mac version

---
+++ Prototype House Mac/App/PrototypeHouseMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_House_MacApp.swift
+//  Prototype House Mac
+//
+//  Created by Felipe Espinoza on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeHouseMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
