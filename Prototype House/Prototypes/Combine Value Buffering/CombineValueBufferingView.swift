0996e7f6bb9fcfbcc55711fa97c0b899fe3eb131
Felipe Espinoza <fespinozacast@gmail.com>
2023-02-06 10:43:01 +0100

    Create CombineValueBufferingView.swift

---
+++ Prototype House/Prototypes/Combine Value Buffering/CombineValueBufferingView.swift
@@ -0,0 +1,55 @@
+//
+//  CombineValueBufferingView.swift
+//  Prototype House
+//
+//  Created by Felipe Espinoza on 06/02/2023.
+//
+
+import SwiftUI
+import Combine
+
+extension CombineValueBufferingView {
+    class ViewModel: ObservableObject {
+        @Published var value: CGFloat
+        @Published var bufferedValue: CGFloat
+
+        private var observations: Set<AnyCancellable> = []
+
+        init(value: CGFloat) {
+            self.value = value
+            self.bufferedValue = value
+
+            self
+                .$value
+//                .debounce(for: .milliseconds(200), scheduler: DispatchQueue.main)
+                .delay(for: .milliseconds(300), scheduler: DispatchQueue.main)
+                .sink(receiveValue: { newValue in
+                    self.bufferedValue = newValue
+                })
+                .store(in: &observations)
+        }
+    }
+}
+
+struct CombineValueBufferingView: View {
+    @StateObject var viewModel: ViewModel = .init(value: 1.0)
+
+    var body: some View {
+        VStack {
+            Slider(value: $viewModel.value)
+
+            Text("Normal: ").bold() +
+            Text(String(format: "%.2f", viewModel.value * 100))
+
+            Text("Buffered: ").bold() +
+                Text(String(format: "%.2f", viewModel.bufferedValue * 100))
+        }
+        .padding()
+    }
+}
+
+struct CombineValueBufferingView_Previews: PreviewProvider {
+    static var previews: some View {
+        CombineValueBufferingView()
+    }
+}
